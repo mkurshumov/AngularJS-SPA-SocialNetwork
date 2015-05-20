317b5863253755d3b2d4d1afbe7d3f72052a41a6
MKURSHUMOV <kur6uma666@gmail.com>
2015-05-20 19:31:56 +0300

    Fixed logout

--- AngularJS/SocialNetwork/js/controllers/AppController.js
+++ AngularJS/SocialNetwork/js/controllers/AppController.js
@@ -2,14 +2,19 @@
 
 // The AppController holds the presentation logic for the entire app (common for all screens)
 app.controller('AppController',
-    function ($http, $scope, $location, authService) {
+    function ($http, $scope, $location, authService, notifyService) {
         // Put the authService in the $scope to make it accessible from all screens
         $scope.authService = authService;
         $scope.location = $location;
+        $scope.notifyService = notifyService;
+        $scope.logout = function () {
+            authService.logout();
+            notifyService.showInfo('Logout Successful');
+            $location.path('/');
+        };
         //$scope.userService = userService;
         //$scope.userSearch = function () {
         //    userService.searchUsersByName();
         //}
-
     }
 );
